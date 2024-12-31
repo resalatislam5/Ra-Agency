5c8dd615837e295af35b7e9102419aa7618e1e73
Resalat islam <resalatislam5@gmail.com>
2024-12-31 21:41:57 +0600

    blog page responsive

--- src/app/blog/Blog.tsx
+++ src/app/blog/Blog.tsx
@@ -1,16 +1,11 @@
-import CommonBanner from "@/components/shared/CommonBanner";
-import Link from "next/link";
-import img from "@/assets/kobu-agency-7okkFhxrxNw-unsplash 1.png";
-import Image from "next/image";
-import facebook from "@/assets/facebook (2).png";
-import twiter from "@/assets/twitter (2).png";
-import intagram from "@/assets/instagram (1).svg";
-import linkedin from "@/assets/linkedin (1) 1.svg";
-import { digitalBrillianceItem } from "@/constants";
-import Tags from "@/app/blog/blogComponents/Tags";
 import Cateories from "@/app/blog/blogComponents/Cateories";
-import RecentPost from "@/app/blog/blogComponents/RecentPost";
 import FollowUs from "@/app/blog/blogComponents/FollowUs";
+import RecentPost from "@/app/blog/blogComponents/RecentPost";
+import Tags from "@/app/blog/blogComponents/Tags";
+import img from "@/assets/kobu-agency-7okkFhxrxNw-unsplash 1.png";
+import CommonBanner from "@/components/shared/CommonBanner";
+import Image from "next/image";
+import Link from "next/link";
 import SearchBar from "./blogComponents/SearchBar";
 function Blog() {
   return (
@@ -18,7 +13,7 @@ function Blog() {
       {/* this is banner section */}
       <CommonBanner pageName="Blog" />
       {/* main blog items */}
-      <section className="max-w-7xl mx-auto grid grid-cols-3 mt-[120px] gap-9">
+      <section className="max-w-7xl mx-auto grid lg:grid-cols-3 mt-[120px] gap-9 px-5">
         {/* left blog items */}
         <div className="col-span-2 flex flex-col gap-10">
           {[...Array(10)].map((e, i) => (
@@ -40,10 +35,10 @@ function Blog() {
               <p className="text-[#CF0] px-5 py-2">September 14, 2023</p>
 
               <div className="flex flex-col gap-7 px-5 ">
-                <h1 className="text-[32px] font-bold">
+                <h1 className="sm:text-[32px] text-xl font-bold">
                   Our Futuristic Marketin Realtor Should Read
                 </h1>
-                <p className="text-lg text-white">
+                <p className="sm:text-lg text-sm text-white">
                   There are endless ways to create a some ways to have a
                   business. We spend more time at themeetings than ever with
                   knowledge melioration. It is a long...
@@ -69,7 +64,7 @@ function Blog() {
           ))}
         </div>
         {/* right blog items */}
-        <div className="col-span-1 flex flex-col gap-[70px]">
+        <div className="col-span-1 lg:flex flex-col gap-[70px] hidden">
           {/* search-bar  */}
           <SearchBar />
           {/* Recent Posts */}
--- src/components/shared/CommonBanner.tsx
+++ src/components/shared/CommonBanner.tsx
@@ -5,10 +5,10 @@ function CommonBanner({ pageName }: { pageName: string }) {
   return (
     <div
       style={{ backgroundImage: `url(${blogImg.src})` }}
-      className="bg-cover bg-no-repeat h-[720px] flex items-center justify-start w-full bg-blend-multiply bg-[#09153099]"
+      className="bg-cover bg-no-repeat sm:h-[720px] h-96 flex items-center justify-start w-full bg-blend-multiply bg-[#09153099]"
     >
-      <div className="flex flex-col justify-center gap-8 w-[1280px] mx-auto">
-        <h2 className="text-6xl font-bold">{pageName}</h2>
+      <div className="flex flex-col justify-center gap-8 w-[1280px] mx-auto px-5">
+        <h2 className="sm:text-6xl text-4xl font-bold">{pageName}</h2>
         <div className="text-lg font-bold flex gap-4 items-center">
           <Link href="/">Home</Link>
           <p className="w-[79px] h-[1px] bg-white"></p>
